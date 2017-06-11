83caedb888a1eb75b7d4c0e39c656bccd9f130ec
soulduck <plznw4me@naver.com>
2017-06-11 17:23:36 +0900

    get cache data that was divide train data test data

--- data.py
+++ data.py
@@ -2,6 +2,10 @@ import numpy as np
 
 def get_cache_data_normal_vs_abnormal(normal_cache_data , abnormal_cache_data ,train_ratio=0.95):
 
+
+    NORMAL_INDEX=0
+    ABNORMAL_INDEX=1
+
     n_normal=len(normal_cache_data)
     n_abnormal=len(abnormal_cache_data)
 
@@ -12,24 +16,48 @@ def get_cache_data_normal_vs_abnormal(normal_cache_data , abnormal_cache_data ,t
 
     train_normal_indics=random.sample(range(n_normal) , n_train_normal)
     test_normal_indics = random.sample(range(n_normal), n_train_normal)
+    train_normal_caches=normal_cache_data[train_normal_indics]
+    test_normal_caches=normal_cache_data[test_normal_indics]
 
     train_abnormal_indics=random.sample(range(n_abnormal) , n_train_abnormal)
     test_abnormal_indics = random.sample(range(n_abnormal), n_train_abnormal)
+    train_abnormal_caches = normal_cache_data[train_abnormal_indics]
+    test_abnormal_caches = normal_cache_data[test_abnormal_indics]
+
+    train_data=np.concatenate((train_normal_caches , train_abnormal_caches),axis=0)
+    test_data=np.concatenate((test_normal_caches , test_abnormal_caches),axis=0)
+
+    normal_labels = np.zeros([n_normal])
+    abnormal_labels = np.ones([n_abnormal])
+
+    train_normal_labels=normal_labels[:n_train_normal]
+    test_normal_labels = normal_labels[n_train_normal:]
+    train_abnormal_labels = abnormal_labels[:n_train_abnormal]
+    test_abnormal_labels = abnormal_labels[:n_train_abnormal]
+    train_labels=np.concatenate((train_normal_labels,train_abnormal_labels))
+    test_labels = np.concatenate((test_normal_labels, test_abnormal_labels))
 
     if __debug__== True:
         print '########noraml############'
         print 'the number of normal :' , n_normal
         print 'the number of train normal :', n_train_normal
         print 'the number of test normal :', n_test_normal
-
+        print 'the shape of train normal caches :',train_normal_caches
+        print 'the shape of test normal caches :',test_normal_caches
         print '########abnoraml##########'
         print 'the number of abnormal :', n_abnormal
         print 'the number of train abnormal :', n_train_abnormal
         print 'the number of test abnormal :', n_test_abnormal
-
+        print 'the shape of train abnormal caches :',train_abnormal_caches
+        print 'the shape of test abnormal caches :',test_abnormal_caches
         print '##########################'
         print 'the total of trianing data:',n_train_normal + n_train_abnormal
-        print 'the total of trianing data:',n_test_normal + n_test_abnormal
+        print 'the total of test data:',n_test_normal + n_test_abnormal
+        print 'the shape of training data shape:' ,train_data.shape
+        print 'the shape of test data shape:' ,test_data.shape
+        print 'the shape of traininn labels shape:', train_labels.shape
+        print 'the shape of test labels shape:', test_labels.shape
+
     return train_data ,train_labels , test_data, test_labels
 
 
